dc2bf81bbd184909b394e9191128fbdaa4f07b23
PankajkJhaRepo <30408282+PankajkJhaRepo@users.noreply.github.com>
2017-08-06 01:17:27 +0530

    initial

---
+++ C2D_Simulator/Program.cs
@@ -0,0 +1,71 @@
+﻿using Common;
+using Microsoft.Azure.Devices.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimulatedDevice
+{
+    class Program
+    {
+        static DeviceClient deviceClient;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simulated device\n");
+            // deviceClient = DeviceClient.CreateFromConnectionString(Constants.connectionString, "myFirstDevice",TransportType.Mqtt);
+            deviceClient = DeviceClient.CreateFromConnectionString(Constants.connectionString, "myFirstDevice");
+            ReceiveC2dAsync();
+
+         //   string connectionString = "HostName=IOTHUBKDIDemo1.azure-devices.net;DeviceId=myFirstDevice;SharedAccessKey=y4Pmw9wRqQpHnrmfgbWkTHq7xfSXIPhElww9eCm1SbA=";
+        //    deviceClient = DeviceClient.CreateFromConnectionString(connectionString);
+        //   ReceiveC2dAsync();
+          
+            //runCommandLoopReceiver();
+        }
+        private static  void ReceiveC2dAsync()
+        {
+            Console.WriteLine("\nReceiving cloud to device messages from service");
+            while (true)
+            {
+                Message receivedMessage =  deviceClient.ReceiveAsync().Result ;
+                if (receivedMessage == null) continue;
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Received message: {0}", Encoding.ASCII.GetString(receivedMessage.GetBytes()));
+                Console.ResetColor();
+
+                deviceClient.CompleteAsync(receivedMessage).Wait() ;
+            }
+        }
+
+        private static void runCommandLoopReceiver()
+        {
+            {
+                // Goto DeviceExplorer and copy copy 
+                // Shared Access Policy for your device. 
+                string connectionString = "HostName=IOTHUBKDIDemo1.azure-devices.net;DeviceId=myFirstDevice;SharedAccessKey=y4Pmw9wRqQpHnrmfgbWkTHq7xfSXIPhElww9eCm1SbA=";
+
+                var deviceClient = DeviceClient.CreateFromConnectionString(connectionString);
+
+                while (true)
+                {
+
+                    var msg = deviceClient.ReceiveAsync().Result;
+                    if (msg != null)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("Received message: {0}", Encoding.ASCII.GetString(msg.GetBytes()));
+                        Console.ResetColor();
+                    }
+
+                    Task.Delay(1000).Wait();
+                }
+
+
+            }
+        }
+    }
+}
---
+++ SendCloudToDevice/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Common;
+
+namespace SendCloudToDevice
+{
+    class Program
+    {
+        static ServiceClient serviceClient;
+        static string connectionString = Constants.connectionString;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Send Cloud-to-Device message\n");
+            serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+
+            Console.WriteLine("Press any key to send a C2D message.");
+            Console.ReadLine();
+            ReceiveFeedbackAsync();
+            SendCloudToDeviceMessageAsync().Wait();
+            Console.ReadLine();
+        }
+        private async static Task SendCloudToDeviceMessageAsync()
+        {
+            var commandMessage = new Message(Encoding.ASCII.GetBytes("Cloud to device message."));
+            commandMessage.Ack = DeliveryAcknowledgement.Full;
+            await serviceClient.SendAsync("myFirstDevice", commandMessage);
+            Console.WriteLine("message sent.");
+        }
+        private async static void ReceiveFeedbackAsync()
+        {
+            var feedbackReceiver = serviceClient.GetFeedbackReceiver();
+
+            Console.WriteLine("\nReceiving c2d feedback from service");
+            while (true)
+            {
+                var feedbackBatch = await feedbackReceiver.ReceiveAsync();
+                if (feedbackBatch == null) continue;
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Received feedback: {0}", string.Join(", ", feedbackBatch.Records.Select(f => f.StatusCode)));
+                Console.ResetColor();
+
+                await feedbackReceiver.CompleteAsync(feedbackBatch);
+            }
+        }
+    }
+}
